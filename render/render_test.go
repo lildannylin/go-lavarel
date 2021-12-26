c60b1ee5140d2fde32ca1cd0c87089fb7191115a
Danny Lin <ayen1201@gmail.com>
2021-12-26 17:37:00 +0800

    Using for loop tests instead

--- render/render_test.go
+++ render/render_test.go
@@ -21,42 +21,31 @@ var pageData = []struct {
 }
 
 func TestRender_Page(t *testing.T) {
-	r, err := http.NewRequest("GET", "/some-url", nil)
-	if err != nil {
-		t.Error(err)
+
+	for _, e := range pageData {
+		r, err := http.NewRequest("GET", "/some-url", nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		w := httptest.NewRecorder()
+
+		testRenderer.Renderer = e.renderer
+		testRenderer.RootPath = "./testdata"
+
+		err = testRenderer.Page(w, r, e.template, nil, nil)
+		if e.errorExpected {
+			if err == nil {
+				t.Errorf("%s: %s", e.name, e.errorMessage, err.Error())
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%s: %s: %s", e.name, e.errorMessage, err.Error())
+
+			}
+		}
 	}
 
-	w := httptest.NewRecorder()
-
-	testRenderer.Renderer = "go"
-	testRenderer.RootPath = "./testdata"
-
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering page", err)
-	}
-
-	err = testRenderer.Page(w, r, "no-file", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existent template", err)
-	}
-
-	testRenderer.Renderer = "jet"
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering page", err)
-	}
-
-	err = testRenderer.Page(w, r, "no-file", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existent template", err)
-	}
-
-	testRenderer.Renderer = ""
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err == nil {
-		t.Error("No Error return while rendering with invalid renderer specified", err)
-	}
 }
 
 func TestRender_GoPage(t *testing.T) {
