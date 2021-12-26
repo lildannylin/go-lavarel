f34fa0a04663e34e968f9d5609aaad7a15943c2a
Danny Lin <ayen1201@gmail.com>
2021-12-26 18:45:06 +0800

    Adding tests for session package

--- middleware.go
+++ middleware.go
@@ -3,5 +3,7 @@ package celeritas
 import "net/http"
 
 func (c *Celeritas) SessionLoad(next http.Handler) http.Handler {
+	c.InfoLog.Println("SessionLoad Called")
+
 	return c.Session.LoadAndSave(next)
 }
