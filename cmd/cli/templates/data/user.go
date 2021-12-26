341f40a73353f563e5f3e56269d6ec0287f3b06d
Danny Lin <ayen1201@gmail.com>
2021-12-26 22:40:14 +0800

    Adding Delete, Insert and ResetPassword func

--- cmd/cli/templates/data/user.go
+++ cmd/cli/templates/data/user.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	up "github.com/upper/db/v4"
+	"golang.org/x/crypto/bcrypt"
 	"time"
 )
 
@@ -89,6 +90,58 @@ func (u *User) Update(theUser User) error {
 	collection := upper.Collection(u.Table())
 	res := collection.Find(theUser.ID)
 	err := res.Update(&theUser)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *User) Delete(id int) error {
+	collection := upper.Collection(u.Table())
+	res := collection.Find(id)
+	err := res.Delete()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *User) Insert(theUser User) (int, error) {
+	newHash, err := bcrypt.GenerateFromPassword([]byte(theUser.Password), 12)
+	if err != nil {
+		return 0, err
+	}
+
+	theUser.CreatedAt = time.Now()
+	theUser.UpdatedAt = time.Now()
+	theUser.Password = string(newHash)
+
+	collection := upper.Collection(u.Table())
+	res, err := collection.Insert(theUser)
+	if err != nil {
+		return 0, err
+	}
+
+	id := getInsertID(res.ID())
+
+	return id, nil
+}
+
+func (u *User) ResetPassword(id int, password string) error {
+	newHash, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	theUser, err := u.Get(id)
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(newHash)
+	err = theUser.Update(*u)
 	if err != nil {
 		return err
 	}
