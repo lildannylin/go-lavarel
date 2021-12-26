fede994db497abb8e6cb6dea5f86de0d97bff631
Danny Lin <ayen1201@gmail.com>
2021-12-26 17:30:00 +0800

    Simplifying tests using table tests

--- render/render_test.go
+++ render/render_test.go
@@ -6,6 +6,20 @@ import (
 	"testing"
 )
 
+var pageData = []struct {
+	name          string
+	renderer      string
+	template      string
+	errorExpected bool
+	errorMessage  string
+}{
+	{"go_page", "go", "home", false, "error rendering go template"},
+	{"go_page_no_template", "go", "no-file", true, "no error rendering non-existent go template, when one is expected"},
+	{"jet_page", "jet", "home", false, "error rendering jet template"},
+	{"jet_page_no_template", "jet", "no-file", true, "no error rendering non-existent jet template, when one is expected"},
+	{"invalid_render_engine", "foo", "home", true, "no error rendering with non-existent template engine"},
+}
+
 func TestRender_Page(t *testing.T) {
 	r, err := http.NewRequest("GET", "/some-url", nil)
 	if err != nil {
