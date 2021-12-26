67b9af219592a0305917463271026aff0e40fe03
Danny Lin <ayen1201@gmail.com>
2021-12-26 22:18:42 +0800

    Updated the token and user operation in db

--- cmd/cli/templates/data/user.go
+++ cmd/cli/templates/data/user.go
@@ -1,6 +1,9 @@
 package data
 
-import "time"
+import (
+	up "github.com/upper/db/v4"
+	"time"
+)
 
 type User struct {
 	ID        int       `db:"id,omitempty"`
@@ -12,4 +15,46 @@ type User struct {
 	CreatedAt time.Time `db:"created_at"`
 	UpdatedAt time.Time `db:"updated_at"`
 	Token     Token     `db:"-"`
+}
+
+func (u *User) Table() string {
+	return "user"
+}
+
+func (u *User) GetAll(condition up.Cond) ([]*User, error) {
+	collection := upper.Collection(u.Table())
+
+	var all []*User
+
+	res := collection.Find(condition)
+	err := res.All(&all)
+	if err != nil {
+		return nil, err
+	}
+
+	return all, nil
+}
+
+func (u *User) GetByEmail(email string) (*User, error) {
+	var theUser User
+	collection := upper.Collection(u.Table())
+	res := collection.Find(up.Cond{"email =": email})
+	err := res.One(&theUser)
+	if err != nil {
+		return nil, err
+	}
+
+	var token Token
+	collection = upper.Collection(token.Table())
+	res = collection.Find(up.Cond{"user_id =": theUser.ID, "expires <": time.Now()}).OrderBy("created_at desc")
+	err = res.One(&token)
+	if err != nil {
+		if err != up.ErrNilRecord && err != up.ErrNoMoreRows {
+			return nil, err
+		}
+	}
+
+	theUser.Token = token
+
+	return &theUser, nil
 }
